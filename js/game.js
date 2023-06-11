d71dcd90ffe288a7e6f9ec410b50b2f7653cf31a
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-06-10 22:55:46 -0500

    inserindo mecanicas de jogo

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ const characters = [
   "jessica",
   "morty",
   "pessoa-passaro",
-  "pickle rick",
+  "pickle-rick",
   "rick",
   "summer",
   "meeseeks",
@@ -20,8 +20,75 @@ function createElement(tag, className) {
   return element;
 }
 
+
+let firstCard = "";
+let secondCard = "";
+
+//Faz uma contagem  continua onde verifica que se todos os pares estão de acordo.
+function checkEndGame() {
+  const disabledCards = document.querySelectorAll(".disabled-card");
+
+  if(disabledCards.length == 20){
+    alert("Parabens você ganhou")
+  }
+}
+
+//fazendo verificação da jogada
+function checkCards() {
+  const firstCharacter = firstCard.getAttribute("data-character");
+  const secondCharacter = secondCard.getAttribute("data-character");
+
+  //se a verificação for valida as cartas ficam viradas e desabilitadas
+  if (firstCharacter == secondCharacter) {
+
+    firstCard.firstChild.classList.add("disabled-card")
+    secondCard.firstChild.classList.add("disabled-card")
+
+    firstCard = "";
+    secondCard = ""
+    //verifica se o jogador venceu o jogo
+    checkEndGame()
+
+    // se não ocorrem a contagem de 500milisegundos e elas voltam a posição inicial
+  }else {
+    setTimeout(() => {
+      firstCard.classList.remove("reveal-card");
+      secondCard.classList.remove("reveal-card");
+
+      firstCard = "";
+      secondCard = "";
+
+    }, 500);
+  }
+
+}
+
+//função para virar carta ao clicar
+function revealCard({ target }) {
+
+  if (target.parentNode.className.includes("reveal-card")) {
+    return;
+  }
+
+  if (firstCard == "") {
+
+    target.parentNode.classList.add("reveal-card")
+    firstCard = target.parentNode
+
+  } else if (secondCard == "") {
+
+    target.parentNode.classList.add("reveal-card")
+    secondCard = target.parentNode
+    
+    checkCards()
+  }
+
+}
+
+
 //criando carta
 function createCard(character) {
+
   const card = createElement("div", "card");
   const front = createElement("div", "face front");
   const back = createElement("div", "face back");
@@ -31,12 +98,22 @@ function createCard(character) {
   card.appendChild(front);
   card.appendChild(back);
 
+  card.addEventListener("click", revealCard)
+  card.setAttribute("data-character", character)
+
   return card;
 }
 
+
 // essa função faz a leitura da lista retornando os personagens nas cartas
 function loadGame() {
-  characters.forEach((character) => {
+  //duplica nossas listas de personagens e jogamos ele na variavel shuffeldArray onde a mesma embaralha
+  const duplicateCharacters = [... characters, ... characters];
+
+  //Emparalha os cadars utilizando o o metodo sort
+  const shuffledArray = duplicateCharacters.sort(() => Math.random()- 0.5)
+
+  shuffledArray.forEach((character) => {
 
     const card = createCard(character);
     grid.appendChild(card);
